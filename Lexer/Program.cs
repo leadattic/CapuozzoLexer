c263079298b7af0cf25a0f43aa81d753bf498c18
leadattic <90087384+leadattic@users.noreply.github.com>
2024-11-04 23:45:54 +0100

    Added a couple more tokentypes

--- Lexer/Program.cs
+++ Lexer/Program.cs
@@ -14,8 +14,30 @@ namespace CapuozzoLanguge
         }
     }
 }
+/*
+ * Regarding all code in the namespace Lexer:
+ * Copyright (c) 2015 Robert Nystrom
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to
+    deal in the Software without restriction, including without limitation the
+    rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+    sell copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
+ */
 namespace Lexer
 {
+    
     class Lexer
     {
         private static int current = 0;
@@ -35,7 +57,7 @@ namespace Lexer
             keywords.Add("for", TokenType.FOR);
             keywords.Add("while", TokenType.WHILE);
             keywords.Add("return", TokenType.RETURN);
-            keywords.Add("func", TokenType.FUNC);
+            keywords.Add("fn", TokenType.FUNC);
             keywords.Add("int", TokenType.INT);
             keywords.Add("var", TokenType.VARDEFINITION);
             
@@ -334,9 +356,13 @@ namespace Lexer
         TRUE = 36,
         VARDEFINITION = 37,
         WHILE = 38,
-        STRDEFINITION = 39,
+        STRDEFINITION = 39, // TODO: Implement these
         INTDEFINITION = 40,
         FLOATDEFINITION = 41,
+        IMPORT = 42,
+        CIMPORT = 43,
+        
+        
 
 // End of file
         EOF = 0
